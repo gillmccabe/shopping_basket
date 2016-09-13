b310f3399f337b18a9e4a9f49f90f8f8df615d90
Gillian McCabe <gillianmccabe@outlook.com>
2016-09-13 18:13:35 +0100

    set up Basket class

---
+++ shopping_basket/Basket.java
@@ -0,0 +1,15 @@
+package shopping_basket;
+import java.util.*;
+
+public class Basket {
+
+  private ArrayList<Item> basket;
+
+  public Basket(){
+    this.basket = new ArrayList<Item>();
+  }
+
+
+
+
+}
